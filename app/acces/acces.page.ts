8b4b621d97e54cc081e08ad47363dc07f1427d33
Alberto Fernandez <albe.fernandez@duocuc.cl>
2022-09-06 16:11:48 -0300

    Experiencia 1

---
+++ app/acces/acces.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-acces',
+  templateUrl: './acces.page.html',
+  styleUrls: ['./acces.page.scss'],
+})
+export class AccesPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
